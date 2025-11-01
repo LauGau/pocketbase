5b09d068fef2af167846f1a745b1bc2fb7fe8f91
Laurent Gaulhiac <laurent@upwwward.io>
2025-11-01 13:22:08 +0700

    fix: pinCollections "shareToken" can now be empty (ex: private col.)

---
+++ pb_migrations/1761978085_updated_pinCollections.js
@@ -0,0 +1,42 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1778662752")
+
+  // update field
+  collection.fields.addAt(6, new Field({
+    "autogeneratePattern": "[a-z0-9]{40}",
+    "hidden": false,
+    "id": "text2115302333",
+    "max": 0,
+    "min": 0,
+    "name": "shareToken",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1778662752")
+
+  // update field
+  collection.fields.addAt(6, new Field({
+    "autogeneratePattern": "[a-z0-9]{40}",
+    "hidden": false,
+    "id": "text2115302333",
+    "max": 0,
+    "min": 0,
+    "name": "shareToken",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": true,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+})
